e07db323c76deb992e6b3fe1ca1fcb0a924bf4c6
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2026-02-20 21:16:04 +0700

    fixed urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -28,6 +29,7 @@ urlpatterns = [
     path("api/", include("network_node.urls")),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("", RedirectView.as_view(url="/swagger/", permanent=False)),
 ]
 
 if settings.DEBUG:
